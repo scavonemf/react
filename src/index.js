9a55d808e9fa7d071cb1876473bab1e46593d620
mflir <scavone.mf@gmail.com>
2021-03-27 23:40:56 -0300

    add apollo and graphql

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider} from 'react-apollo'
+
 import { App } from './App'
 
-ReactDOM.render(<App />, document.getElementById('app'))
+const client = new ApolloClient({
+  uri: 'https://petgram-server-jrmfsd-okxluew9o.now.sh/graphql'
+})
+
+ReactDOM.render(
+  // eslint-disable-next-line react/jsx-filename-extension
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('app')
+)
